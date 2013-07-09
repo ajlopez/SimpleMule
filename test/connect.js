77d10d91451a150513b68c50a0a551956d1db666
ajlopez <webmaster@ajlopez.com>
2013-07-09 05:23:54 -0300

    Context post first test

--- test/connect.js
+++ test/connect.js
@@ -17,3 +17,22 @@ exports['Connect two components'] = function (test) {
     
     increment.send(1);
 }
+
+exports['Connect two components using post'] = function (test) {
+    test.expect(3);
+    
+    var increment = sm.createComponent(function (context, message) {
+        context.post('next', message + 1);
+    });
+    
+    var component = sm.createComponent(function (context, message) {
+        test.ok(context);
+        test.ok(message);
+        test.equal(message, 2);
+        test.done();
+    });
+    
+    increment.connect('next', component);
+    
+    increment.send(1);
+}
