ee3c1d154386c68a4c161711b65628cce7162370
ajlopez <webmaster@ajlopez.com>
2013-07-09 05:30:28 -0300

    Connect one component to two components

--- test/connect.js
+++ test/connect.js
@@ -34,5 +34,75 @@ exports['Connect two components using post'] = function (test) {
     
     increment.connect('next', component);
     
+    increment.post(1);
+}
+
+exports['Connect one component to two components'] = function (test) {
+    var increment = sm.createComponent(function (context, message) {
+        context.send('next', message + 1);
+    });
+    
+    var ntimes = 0;
+    
+    var component1 = sm.createComponent(function (context, message) {
+        test.ok(context);
+        test.ok(message);
+        test.equal(message, 2);
+        ntimes++;
+        
+        if (ntimes == 2)
+            test.done();
+    });
+    
+    var component2 = sm.createComponent(function (context, message) {
+        test.ok(context);
+        test.ok(message);
+        test.equal(message, 2);
+        ntimes++;
+        
+        if (ntimes == 2)
+            test.done();
+    });
+    
+    increment.connect('next', component1);
+    increment.connect('next', component2);
+    
     increment.send(1);
 }
+
+exports['Connect one component to two components using post'] = function (test) {
+    test.expect(6);
+
+    var increment = sm.createComponent(function (context, message) {
+        context.post('next', message + 1);
+    });
+    
+    var ntimes = 0;
+    
+    var component1 = sm.createComponent(function (context, message) {
+        test.ok(context);
+        test.ok(message);
+        test.equal(message, 2);
+        ntimes++;
+        
+        if (ntimes == 2)
+            test.done();
+    });
+    
+    var component2 = sm.createComponent(function (context, message) {
+        test.ok(context);
+        test.ok(message);
+        test.equal(message, 2);
+        ntimes++;
+        
+        if (ntimes == 2)
+            test.done();
+    });
+    
+    increment.connect('next', component1);
+    increment.connect('next', component2);
+    
+    increment.post(1);
+}
+
+
