6e3f815d830df777edff6ad41de982136ad88a69
ajlopez <webmaster@ajlopez.com>
2014-07-15 10:33:50 -0300

    Simple send first test

--- test/flow.js
+++ test/flow.js
@@ -1,10 +1,18 @@
 
 var sm = require('..');
 
-exports['Create flow'] = function (test) {
+exports['create flow'] = function (test) {
     var flow = sm.flow();
     
     test.ok(flow);
     test.equal(typeof flow, 'object');
     test.done();
 }
+
+exports['simple send'] = function (test) {
+    var flow = sm.flow();
+
+    test.equal(1, flow.send(1));
+    test.done();
+}
+
